03172ae963f04f5887f4ae4fd19b1ebb26066f69
xunle <xunle1121@gamil.com>
2022-12-15 00:50:03 +0800

    ch03-variables

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Rust
----
+# 变量和数据类型
 
-This repository is projects of learning rust.
+## 变量
+
+### 可变性
+变量默认是不可变（immutable）的，使用 `mut` 可以是变量可变
+
+### 常量
+使用 `const` 关键字声明常量，需指定类型和值，可以在任意作用域声明
+
+## 数据类型
+rust 有两种数据类型子集：标量（scalar）和复合（compound）
+
+###
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    // assign a mutable variable
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+    // constant
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // println!("three hours equals {THREE_HOURS_IN_SECONDS} seconds")
 }
