e8c1db35de32b0768906753028d951486d1981e8
xunle <xunle1121@gmail.com>
2023-11-23 00:57:49 +0800

    add generics and trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("default impl")
+    }
+    fn describe(&self) {
+        println!("decribe Summary")
+    }
+}
+
+struct Article {
+    content: String,
+}
+
+impl Summary for Article {
+    fn summarize(&self) -> String {
+        format!("{}", self.content)
+    }
+}
+
+fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Article {
+        content: String::from("test"),
+    };
+    a.describe();
+    println!("{}", a.summarize());
+    notify(&a);
 }
